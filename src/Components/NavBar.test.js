93bb1f2f76f181fde360a583055cb314c5e69d7a
Josh Goldberg <josh@codecademy.com>
2021-03-22 13:46:21 -0400

    More informative README.md

--- src/Components/NavBar.test.js
+++ src/Components/NavBar.test.js
@@ -4,15 +4,16 @@ import React from "react";
 import { NavBar } from "./NavBar";
 
 describe("NavBar", () => {
-  test.each([[0, "What is Pursuit?", 1, "Create an account", 2, "Sign in"]])(
-    "child at %i is a button with %s",
-    (index, text) => {
-      const wrapper = shallow(<NavBar />);
+  test.each([
+    [0, "What is Pursuit?"],
+    [1, "Create an account"],
+    [2, "Sign in"],
+  ])("child at %i is a button with %s", (index, text) => {
+    const wrapper = shallow(<NavBar />);
 
-      const child = wrapper.childAt(index);
+    const child = wrapper.childAt(index);
 
-      expect(child).toHaveDisplayName("button");
-      expect(child).toHaveText(text);
-    }
-  );
+    expect(child).toHaveDisplayName("button");
+    expect(child).toHaveText(text);
+  });
 });
